8e6889c890b9ff877808969f62a29860dbe26001
LOKI-08 <yashlakas08@gmail.com>
2025-02-01 11:49:46 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold text-blue-600">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
